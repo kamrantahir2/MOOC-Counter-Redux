d15c597ee0aae4190d34b7f0aedf69d2bb019e75
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-02-18 00:00:27 +0000

    Refactor code by moving all Context code to CounterContext, Create CounterContextProvider component in CounterContext and implement in main.jsx to give entire app access to CounterContext

--- src/App.jsx
+++ src/App.jsx
@@ -2,42 +2,29 @@ import { useReducer } from "react";
 import "./App.css";
 import CounterContext from "./CounterContext";
 import { useContext } from "react";
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    case "DEC":
-      return state - 1;
-    case "ZERO":
-      return 0;
-    default:
-      return state;
-  }
-};
+import { useCounterDispatch } from "./CounterContext";
+import { useCounterValue } from "./CounterContext";
 
 const Display = () => {
-  const [counter] = useContext(CounterContext);
+  const counter = useCounterValue();
   return <div>{counter}</div>;
 };
 
 const Button = ({ type, label }) => {
-  const [counter, dispatch] = useContext(CounterContext);
+  const dispatch = useCounterDispatch();
   return <button onClick={() => dispatch({ type })}>{label}</button>;
 };
 
 function App() {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0);
-
   return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
-      <Display counter={counter} />
+    <div>
+      <Display />
       <div>
-        <Button dispatch={counterDispatch} type="INC" label={"+"} />
-        <Button dispatch={counterDispatch} type="DEC" label={"-"} />
-        <Button dispatch={counterDispatch} type="ZERO" label={"0"} />
+        <Button type="INC" label={"+"} />
+        <Button type="DEC" label={"-"} />
+        <Button type="ZERO" label={"0"} />
       </div>
-    </CounterContext.Provider>
+    </div>
   );
 }
 
